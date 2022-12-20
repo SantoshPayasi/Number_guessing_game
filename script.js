59df2766a64a3b66e776ce867ef6ffa90aa020cb
SantoshPayasi <Spayasi78@gmail.com>
2022-12-21 00:18:48 +0530

    added first part of javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('number');
+const submit = document.querySelector('.check')
+const newGame = document.querySelector('.newgame')
+let tries = document.querySelector('.value')
+let score = document.querySelector('.values')
+let hidden_number=Math.floor(Math.random()*100);
+let highScore = document.getElementById('Highscore')
+let trys =20
+let Score =0
+let Highscore = 0;
+const newgame=()=>{
+    hidden_number=Math.floor(Math.random()*100);
+    tries.innerHTML = trys ;
+    score.innerHTML = Score;
+    console.log(hidden_number)
+  }
+newgame();
+newGame.addEventListener('click', newgame)
+function Check()
+{ if(trys!=0)
+    {
+    if(input.value===hidden_number){
+        score = tries
+    }
+    else{
+       trys--;
+       tries.innerHTML = trys;
+    }
+}
+else{
+if(score<Highscore){
+    highScore.innerHTML = score
+}
+}
+}
+submit.addEventListener('click', Check)
