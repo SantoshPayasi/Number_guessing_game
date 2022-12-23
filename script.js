e57fff2f4ebb0346a5ecf63c0a9086c87785c027
SantoshPayasi <102188325+SantoshPayasi@users.noreply.github.com>
2022-12-23 12:22:22 -0800

    logic has been completed

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
 const input = document.getElementById('number');
 const submit = document.querySelector('.check')
 const newGame = document.querySelector('.newgame')
+let shownumber = document.querySelector('.gussing-num')
 let tries = document.querySelector('.value')
 let score = document.querySelector('.values')
 let hidden_number=Math.floor(Math.random()*100);
-let highScore = document.getElementById('Highscore')
-let trys =20
+let highScore = document.querySelector('#Highscore')
+let trys =10
 let Score =0
 let Highscore = 0;
+
 const newgame=()=>{
+    shownumber.innerHTML='?'
+    trys=10
+    input.disabled=false;
+    input.innerHTML=''
     hidden_number=Math.floor(Math.random()*100);
-    tries.innerHTML = trys ;
+    tries.innerHTML = trys;
     score.innerHTML = Score;
+    console.log(hidden_number)
+    highScore.innerHTML=Highscore
   }
+  const sethigh =( High, Sc)=>{
+    if(High==0){
+        highScore.innerHTML = Sc
+        Highscore=Sc
+     } 
+     else{
+     if(Sc<High){
+        highScore.innerHTML = 0
+     }
+  }
+}
 newgame();
 newGame.addEventListener('click', newgame)
 const Check=()=>
-{ if(trys!=0)
+{ if(trys>0)
     {
-    if(input.value===hidden_number){
-        score = tries
+    if(input.value==hidden_number){
+        --trys;
+        Score=10-trys
+        shownumber.innerHTML=hidden_number
+        tries.innerHTML = trys;
+        score.innerHTML= Score;
+        sethigh(Highscore, Score)
+        input.disabled=true; 
     }
     else{
-       trys--;
+       --trys;
        tries.innerHTML = trys;
+       if(input.value<hidden_number){
+        alert("number is smaller then hidden number")
+       }
+       else{
+        alert("number is higher then hidden number")
+       }
     }
+    
 }
 else{
-if(score<Highscore){
-    highScore.innerHTML = score
+     input.disabled=true;
+     sethigh(Highscore, Score)
+    }
 }
-}
-}
+
 submit.addEventListener('click', Check)
